aa1d14006802570225f6af637c62b36b816bd999
nehakatekar <nehakatekar230@gmail.com>
2022-01-25 19:52:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express= require("express")
+const cookieParser= require("cookie-parser")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const app= express();
+const errorMiddleware = require("./middleware/error");
+dotenv.config({path:"backend/config/config.env"})
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}))
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+app.use(errorMiddleware);
+module.exports =app
